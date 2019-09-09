f7f39870b7c3511682d4370e4b26399d53221bc9
Muhammad Alabed <mohmmadalabed@gmail.com>
2019-09-09 11:02:07 +0300

    add html to process 3d response

--- PayfullClient/src/Payfull/Responses/Responses.php
+++ PayfullClient/src/Payfull/Responses/Responses.php
@@ -1,18 +1,14 @@
 <?php
-
 namespace Payfull\Responses;
-
-
 class Responses
 {
     public static function processResponse($response)
     {
         return json_decode($response,TRUE);
     }
-
     public static function process3DResponse($response)
     {
-        if(strpos($response, '<form'))
+        if(strpos($response, '<form') !== False OR strpos($response, '<html') !== False)
         {
             echo $response;
             exit;
